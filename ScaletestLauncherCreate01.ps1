c663dfb17e5adf47027328ad0287f3c68ac9075c
bparkhill <b.parkhill@loginvsi.com>
2020-12-30 14:53:13 -0700

    Modified to create 3 launchers 11 - 13, throttle at 5 and add descriptor "$using:*"

--- ScaletestLauncherCreate01.ps1
+++ ScaletestLauncherCreate01.ps1
@@ -5,17 +5,27 @@ $Location = "eastus2"
 $VnetName = "BP-EastUS2-ST-VNET"
 $cred = New-Object PSCredential("blair", (ConvertTo-SecureString -AsPlainText -Force -String "LoginVSIlab!1"))
  
-$launchers = @()
-for ($i = 1; $i -le 2; $i++) {
-    $launchers += "BP-Launch{0:D3}" -f $i
-}
-$launchers | foreach-object -ThrottleLimit 10 -Parallel {
+# $launchers = @()
+# for ($i = 1; $i -le 10; $i++) {
+#     $launchers += "BP-Launch{0:D3}" -f $i
+# }
+
+# let's start from 11 since 10 are already built
+ $launchers = @()
+ for ($i = 11; $i -le 13; $i++) {
+     $launchers += "BP-Launch{0:D3}" -f $i
+ }
+
+# Changed throttle limit from 10 to 5 because some launchers didn't get installed
+# due to what I believe are race conditions
+$launchers | foreach-object -ThrottleLimit 5 -Parallel {
     # local Admin credentials to be set on the VM
     #New-AzVM -Name BP-Launch001 -ResourceGroupName "bp-scaletest" -Image "MicrosoftWindowsDesktop:Windows-10:20h1-pro:19041.685.2012032305" -Size Standard_D8as_v4 -Location "eastus2" -Credential $cred
-    New-AzVM -Name $_ -ResourceGroupName $resourcegroup -Image $ImageName -Size $MachineSize -Location $Location -VirtualNetworkName $VnetName -Credential $cred
-    Set-AzVMCustomScriptExtension -ResourceGroupName $resourcegroup `
+    #New-AzVM -Name $_ -ResourceGroupName $using:resourcegroup -Image $using:ImageName -Size $using:MachineSize -Location $using:Location -VirtualNetworkName $using:VnetName -Credential $using:cred
+    New-AzVM -Name $_ -ResourceGroupName $using:resourcegroup -Image $using:ImageName -Size $using:MachineSize -Location $using:Location -Credential $using:cred
+    Set-AzVMCustomScriptExtension -ResourceGroupName $using:resourcegroup `
         -VMName $_ `
-        -Location $location `
+        -Location $using:Location `
         -FileUri https://raw.githubusercontent.com/blairparkhill/WVDConnect/master/ScaletestLauncherBuild01.ps1 `
         -Run ScaletestLauncherBuild01.ps1 `
         -Name Launcher
