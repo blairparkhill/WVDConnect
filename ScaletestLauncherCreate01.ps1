42be6370f85e997dabead7a60d175d04055fc80d
bparkhill <b.parkhill@loginvsi.com>
2020-12-29 17:49:45 -0700

    added vnetname

--- ScaletestLauncherCreate01.ps1
+++ ScaletestLauncherCreate01.ps1
@@ -1,6 +1,9 @@
 $resourcegroup = "bp-scaletest"
-$location = "eastus2"
 $ImageName = "MicrosoftWindowsDesktop:Windows-10:20h1-pro:19041.685.2012032305"
+$MachineSize = "Standard_D8as_v4"
+$Location = "eastus2"
+$VnetName = "BP-EastUS2-ST-VNET"
+$cred = New-Object PSCredential("blair", (ConvertTo-SecureString -AsPlainText -Force -String "LoginVSIlab!1"))
  
 $launchers = @()
 for ($i = 1; $i -le 2; $i++) {
@@ -8,9 +11,8 @@ for ($i = 1; $i -le 2; $i++) {
 }
 $launchers | foreach-object -ThrottleLimit 10 -Parallel {
     # local Admin credentials to be set on the VM
-    $cred = New-Object PSCredential("blair", (ConvertTo-SecureString -AsPlainText -Force -String "LoginVSIlab!1"))
     #New-AzVM -Name BP-Launch001 -ResourceGroupName "bp-scaletest" -Image "MicrosoftWindowsDesktop:Windows-10:20h1-pro:19041.685.2012032305" -Size Standard_D8as_v4 -Location "eastus2" -Credential $cred
-    New-AzVM -Name $_ -ResourceGroupName $resourcegroup -Image $ImageName -Size Standard_D8as_v4 -Location $location -Credential $cred
+    New-AzVM -Name $_ -ResourceGroupName $resourcegroup -Image $ImageName -Size $MachineSize -Location $Location -VirtualNetworkName $VnetName -Credential $cred
     Set-AzVMCustomScriptExtension -ResourceGroupName $resourcegroup `
         -VMName $_ `
         -Location $location `
